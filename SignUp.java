115402b0f31879f760d79f4e93652d88baa1bd1c
thanhnguyenz1 <52021285+thanhnguyenz1@users.noreply.github.com>
2019-06-21 12:30:17 +0700

    Add files via upload

--- SignUp.java
+++ SignUp.java
@@ -3,11 +3,16 @@ package com.tgbao2004.caro.account;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import com.tgbao2004.caro.utils.Dialog;
+import com.tgbao2004.caro.utils.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import java.io.IOException;
 import java.net.URL;
@@ -16,28 +21,104 @@ import java.util.ResourceBundle;
 public class SignUp implements Initializable {
 
     @FXML
-    private JFXTextField userName;
-    public JFXPasswordField cpassword; //confirm password
-    public JFXPasswordField password;
-    public JFXButton createAccount;
+    private JFXTextField inputUserName;
+    public JFXPasswordField inputPassword;
+    public JFXPasswordField inputPasswordConfirm;
+    public JFXButton signUpButton;
     public JFXButton backButton;
+    public Thread thread;
 
-    // Initialize
+    public SignUp() {
+
+    }
+
+    /**
+     * Initialize when scene are created
+     *
+     * @param location
+     * @param resources
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // buttonSubmit will be visiabled if player didn't fill account & password
+        showSignUpButton();
     }
 
-    // Get Parent
-    public static Scene getRootParent() throws IOException {
-        return new Scene(FXMLLoader.load(LoginGui.class.getResource("LoginGui.fxml")));
+
+    /**
+     * Get scene of SignUp from file
+     *
+     * @return
+     * @throws IOException
+     */
+    public Scene getRootParent() throws IOException {
+        return new Scene(FXMLLoader.load(Login.class.getResource("SignUp.fxml")));
     }
 
-    public void createAccountClicked(MouseEvent mouseEvent) {
 
+    /**
+     * Will call when player click back button
+     *
+     * @param actionEvent
+     * @throws IOException
+     */
+    public void onClickedBackButton(ActionEvent actionEvent) throws IOException {
+        discard();
+        Utils.changeScene(new Login().getRootParent());
     }
 
-    public void backButtonClicked(MouseEvent mouseEvent) {
 
+    /**
+     * Will call when player click sign up button
+     *
+     * @param actionEvent
+     */
+    public void onClickedSignUpButton(ActionEvent actionEvent) throws IOException {
+        Dialog dialog = Dialog.newDialog();
+        //Do some thing with this Dialog
+
+        //Update dialog
+        dialog.updateDialog(new Label("Loading..."), new ImageView(new Image("com/tgbao2004/caro/loading.gif")));
+    }
+
+
+    /**
+     * Use to show/hide sign up button if player fill/unfill input
+     */
+    public void showSignUpButton() {
+        boolean[] isFilledUserName = {false};
+        boolean[] isFilledPassword = {false};
+        boolean[] isFilledPasswordConfirm = {false};
+        inputUserName.textProperty().addListener((observable, oldValue, newValue) -> {
+            isFilledUserName[0] = !newValue.trim().isEmpty();
+        });
+        inputPassword.textProperty().addListener((observable, oldValue, newValue) -> {
+            isFilledPassword[0] = !newValue.trim().isEmpty();
+        });
+        inputPasswordConfirm.textProperty().addListener((observable, oldValue, newValue) -> {
+            isFilledPasswordConfirm[0] = !newValue.trim().isEmpty();
+        });
+
+        // Initialize thread to set disable of button
+        thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    signUpButton.setDisable(!(isFilledUserName[0] && isFilledPassword[0] && isFilledPasswordConfirm[0]));
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        thread.start();
+    }
+
+    /**
+     * Will call when player change sence
+     */
+    public void discard() {
+        thread.stop();
     }
 }
